be67d02963b998156a95b8ddffd12c12d1402a69
jubayeralfaruk <jubayeralfaruk@gmail.com>
2025-11-11 00:15:24 +0600

    partners data show in Database to Server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 // middleware
@@ -28,16 +28,35 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const db =  client.db("studyMateDB");
+    const partnersCollection = db.collection("partners");
+
+
+
+    app.get("/partners", async(req, res) => {
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.email = email;
+      }
+      const cursor = partnersCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
