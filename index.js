13fbea72fb7d028598acd85df4069388b8fe132a
jubayeralfaruk <jubayeralfaruk@gmail.com>
2025-11-12 10:44:24 +0600

    update code /partners/:id/increase

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 // console.log(process.env);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dzgjhab.mongodb.net/?appName=Cluster0`;
@@ -17,24 +17,23 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-app.get('/', (req, res) => {
-    res.send("Server is runing now")
-})
+app.get("/", (req, res) => {
+  res.send("Server is runing now");
+});
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const db =  client.db("studyMateDB");
+    const db = client.db("studyMateDB");
     const partnersCollection = db.collection("partners");
-    const partnersRequestCollection = db.collection("partners-request")
-    const usersCollection = db.collection("users")
+    const partnersRequestCollection = db.collection("partners-request");
+    const usersCollection = db.collection("users");
 
-
-    app.get("/partners", async(req, res) => {
+    app.get("/partners", async (req, res) => {
       const email = req.query.email;
       const query = {};
       if (email) {
@@ -43,63 +42,102 @@ async function run() {
       const cursor = partnersCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
-    })
+    });
 
-    app.get("/topPartners", async(req, res) => {
-      const cursor = partnersCollection.find().sort({rating: -1}).limit(6);
+    app.get("/topPartners", async (req, res) => {
+      const cursor = partnersCollection.find().sort({ rating: -1 }).limit(6);
       const result = await cursor.toArray();
-      res.send(result)
-    }) 
+      res.send(result);
+    });
 
-    app.get('/partners/:id', async(req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id)}
-      const result = await partnersCollection.findOne(query)
-      res.send(result)
-    })
-
-    app.post('/partners', async(req, res) => {
-      const partner = req.body;
-      const result = await partnersCollection.insertOne(partner)
-      res.send(result)
-    })
-
-    app.patch("/partners/:id", async(req, res) => {
-      const id = req.params.id;
-      const partner = req.body;
-      const query = { _id: new ObjectId(id)};
-      const update = {
-        $set: partner
-      }
-      const result = await partnersCollection.updateOne(query, update)
-      res.send(result)
-    })
-
-    app.patch("/partners/:id/increase", async(req, res) => {
+    app.get("/partners/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
+      const result = await partnersCollection.findOne(query);
+      res.send(result);
+    });
 
-      const result = await partnersCollection.updateOne(query, {
-        $inc: {patnerCount: 1}
-      })
-    })
+    app.post("/partners", async (req, res) => {
+      const partner = req.body;
+      const result = await partnersCollection.insertOne(partner);
+      res.send(result);
+    });
 
-    app.delete('/partners/:id', async(req, res) => {
+    app.patch("/partners/:id", async (req, res) => {
+      const id = req.params.id;
+      const partner = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: partner,
+      };
+      const result = await partnersCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+    // app.patch("/partners/:id/increase", async(req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+
+    //   const result = await partnersCollection.updateOne(
+    //     query,
+    //     { $inc: { patnerCount: 1 }},
+    //     { returnDocument: "after" }
+    //   )
+    //   res.send(result)
+    // })
+
+    app.patch("/partners/:id/increase", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+
+        const updated = await partnersCollection.findOneAndUpdate(
+          query,
+          { $inc: { partnerCount: 1 } },
+          { returnDocument: "after" }
+        );
+
+        if (!updated.value) {
+          return res.status(404).json({
+            success: false,
+            message: "Partner not found",
+          });
+        }
+
+        res.json({
+          success: true,
+          message: "Partner count increased!",
+          partners: updated.value,
+        });
+
+        res.send(result)
+      } catch (error) {
+        res.status(500).json({
+          success: false,
+          error: error.message,
+        });
+      }
+    });
+
+    app.delete("/partners/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await partnersCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
-    app.get('/partners-request', async(req, res) => {
-      const email = req.params.email;
-      // const query = {  }
-      const cursor = partnersRequestCollection.find();
+    app.get("/partners-request", async (req, res) => {
+      const partnerId = req.query.partnerId;
+      const query = {};
+      if (partnerId) {
+        query.partnerId = partnerId;
+      }
+      const cursor = partnersRequestCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
-    })
+    });
 
-    app.post("/partners-request", async(req, res) => {
+    app.post("/partners-request", async (req, res) => {
       const request = req.body;
 
       const email = req.body.senderEmail;
@@ -112,35 +150,34 @@ async function run() {
       const existingEmail = await partnersRequestCollection.findOne(queryEmail);
 
       if (existingEmail && existingId) {
-        res.send('Sender already send')
+        res.send("Sender already send");
       } else {
         const result = await partnersRequestCollection.insertOne(request);
-        res.send(result)
+        res.send(result);
       }
+    });
 
-    })
-
-    app.patch("/partners-request/:id", async(req, res) => {
+    app.patch("/partners-request/:id", async (req, res) => {
       const id = req.params.id;
       const request = req.body;
       const query = { _id: new ObjectId(id) };
       const update = {
-        $set: request
-      }
+        $set: request,
+      };
       const result = await partnersRequestCollection.updateOne(query, update);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    app.delete("/partners-request/:id", async(req, res) => {
+    app.delete("/partners-request/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await partnersRequestCollection.deleteOne(query);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     //---------------------------------------------------------------------------
-    
-    app.get('/users', async() => {
+
+    app.get("/users", async () => {
       const email = req.query.email;
       const query = {};
       if (email) {
@@ -149,28 +186,26 @@ async function run() {
       const cursor = usersCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
-    })
+    });
 
-    app.post('/users', async(req, res) => {
+    app.post("/users", async (req, res) => {
       const user = req.body;
-      const email = req.body.email
-      const existingEmail = usersCollection.findOne({email:email});
+      const email = req.body.email;
+      const existingEmail = usersCollection.findOne({ email: email });
 
       if (existingEmail) {
-        res.send('User already existing.')
+        res.send("User already existing.");
       } else {
-        const result = await usersCollection.insertOne(user)
-        res.send(result)
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
       }
-
-    })
-
-
-    
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -179,6 +214,5 @@ async function run() {
 run().catch(console.dir);
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-    
-})
+  console.log(`App listening on port ${port}`);
+});
